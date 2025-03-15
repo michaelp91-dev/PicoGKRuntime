961ba76a922f5abb3e90d87ce228d66078e27189
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-15 10:08:01 -0500

    Update termux.sh

--- Install_Dependencies/termux.sh
+++ Install_Dependencies/termux.sh
@@ -7,30 +7,6 @@ pkg update -y
 pkg upgrade -y
 
 # Install necessary packages. Termux uses pkg instead of apt.
-pkg install -y \
-    wget \
-    make \
-    clang \
-    clang-tools \
-    cmake \ 
-    libx11 \
-    libxrandr \
-    libxinerama \
-    libxcursor \
-    libxi \
-    boost \  
-    tbb \    
-    openssl \
-    zeromq \ 
-    jemalloc \
-    blosc \  
-    doxygen \
-    qt5-default \ 
-    dotnet-sdk-7.0 \ 
-    wayland \
-    wayland-protocols \
-    pkg-config \
-    libxkbcommon \
-    llvm 
+pkg install -y wget make clang clang-tools cmake libx11 libxrandr libxinerama libxcursor libxi boost tbb openssl zeromq jemalloc blosc doxygen qt5-default dotnet-sdk-7.0 wayland wayland-protocols pkg-config libxkbcommon llvm 
 
 echo "Dependencies installation complete"
