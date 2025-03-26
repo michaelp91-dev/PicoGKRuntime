cc1e05a6d822f208cabb47160dbc39adeebb5e6c
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-26 11:44:55 -0500

    Update termux.sh

--- Install_Dependencies/termux.sh
+++ Install_Dependencies/termux.sh
@@ -6,16 +6,8 @@
 pkg update -y
 pkg upgrade -y
 
-# Function to search for a package
-search_package() {
-    local search_term="$1"
-    pkg search "$search_term"
-}
-
-
-# libjemalloc
-echo "Searching for jemalloc related packages..."
-search_package jemalloc
+# setting up x11-repo
+pkg install -y x11-repo
 
 # Install packages directly
 pkg install -y wget cmake make clang libx11 libxrandr libxi boost openssl pkg-config llvm libtbb libblosc dotnet-sdk-8.0 libwayland libwayland-protocols libzmq libxcursor libxkbcommon libxinerama
