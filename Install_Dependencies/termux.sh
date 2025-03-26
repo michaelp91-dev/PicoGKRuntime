99868d7b378dece87b2f54abf85fd1dbca6173b6
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-26 11:50:08 -0500

    Update termux.sh

--- Install_Dependencies/termux.sh
+++ Install_Dependencies/termux.sh
@@ -10,6 +10,6 @@ pkg upgrade -y
 pkg install -y x11-repo
 
 # Install packages directly
-pkg install -y wget cmake make clang libx11 libxrandr libxi boost openssl pkg-config llvm libtbb libblosc dotnet-sdk-8.0 libwayland libwayland-protocols libzmq libxcursor libxkbcommon libxinerama
+pkg install -y wget cmake make clang libx11 libxrandr libxi boost openssl pkg-config llvm libtbb libblosc dotnet-sdk-8.0 libwayland libwayland-protocols libzmq libxcursor libxkbcommon libxinerama termux-x11-nightly
 
 echo "Dependencies installation attempt complete. Please review the output for errors and warnings."
