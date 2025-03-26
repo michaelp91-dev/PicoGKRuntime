7981edde257ba768e44becd96ddee52a18185732
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-26 13:08:52 -0500

    Update termux.sh

--- Install_Dependencies/termux.sh
+++ Install_Dependencies/termux.sh
@@ -11,5 +11,5 @@ pkg install -y x11-repo
 
 # Install packages directly
 pkg install -y wget cmake make clang libx11 libxrandr libxi boost openssl pkg-config llvm libtbb libblosc dotnet-sdk-8.0 libwayland libwayland-protocols libzmq libxcursor libxkbcommon libxinerama termux-x11-nightly
-pkg install boost boost-headers libtool autoconf automake
+pkg install -y boost boost-headers libtool autoconf automake
 echo "Dependencies installation attempt complete."
