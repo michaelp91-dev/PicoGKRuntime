35e1509aa7115ea2c54cc5eec0067c1e2148b265
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-26 13:04:20 -0500

    Update and rename compile_termux.sh to termux_build.sh

--- Compile/compile_termux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd PicoGKRuntime
-mkdir termux_build
-sed -i '/project(OpenVDB)/a set(CMAKE_POSITION_INDEPENDENT_CODE ON)' openvdb/CMakeLists.txt
-sed -i '/project(OpenVDB)/a set(BUILD_SHARED_LIBS ON)' openvdb/CMakeLists.txt
-cd termux_build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/../.. ..
-make VERBOSE=1
