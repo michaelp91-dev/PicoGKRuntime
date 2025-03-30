c2479d94af346f81fef47e77766b8c83f96b6cb2
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-03-30 13:51:18 -0500

    Update colab_build.sh

--- Build/colab_build.sh
+++ Build/colab_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd PicoGKRuntime
-rm -r colab_build
-mkdir colab_build
+rm -r build
+mkdir build
 sed -i '/project(OpenVDB)/a set(CMAKE_POSITION_INDEPENDENT_CODE ON)' openvdb/CMakeLists.txt
 sed -i '/project(OpenVDB)/a set(BUILD_SHARED_LIBS ON)' openvdb/CMakeLists.txt
-cd colab_build
+cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/../.. ..
 make
